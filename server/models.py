47fd32a0f5cf6883245c77c9fed4d802b1480f5b
salma <swsalama518@gmail.com>
2026-06-19 21:19:41 -0400

    update

--- server/models.py
+++ server/models.py
@@ -3,10 +3,11 @@ from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
 from marshmallow import Schema, fields
 
-
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
@@ -16,6 +17,13 @@ class Customer(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+
+    reviews = db.relationship(
+        'Review',
+        back_populates='customer'
+    )
+
+    items = association_proxy('reviews', 'item')
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
@@ -28,5 +36,84 @@ class Item(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship(
+        'Review',
+        back_populates='item'
+    )
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    comment = db.Column(db.String)
+
+    customer_id = db.Column(
+        db.Integer,
+        db.ForeignKey('customers.id')
+    )
+
+    item_id = db.Column(
+        db.Integer,
+        db.ForeignKey('items.id')
+    )
+
+    customer = db.relationship(
+        'Customer',
+        back_populates='reviews'
+    )
+
+    item = db.relationship(
+        'Item',
+        back_populates='reviews'
+    )
+
+    def __repr__(self):
+        return f'<Review {self.id}>'
+    
+class CustomerSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+    reviews = fields.Nested(
+        'ReviewSchema',
+        many=True,
+        exclude=('customer',)
+    )
+
+    items = fields.Nested(
+        'ItemSchema',
+        many=True,
+        exclude=('reviews',)
+    )
+
+
+class ItemSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    price = fields.Float()
+
+    reviews = fields.Nested(
+        'ReviewSchema',
+        many=True,
+        exclude=('item',)
+    )
+
+
+class ReviewSchema(Schema):
+    id = fields.Int()
+    comment = fields.Str()
+
+    customer = fields.Nested(
+        'CustomerSchema',
+        exclude=('reviews', 'items')
+    )
+
+    item = fields.Nested(
+        'ItemSchema',
+        exclude=('reviews',)
+    )
